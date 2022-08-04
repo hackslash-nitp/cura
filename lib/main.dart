2efc302e74f64fa7d1e71c1c397b228cbb3de477
rani <rkcrj100@gmail.com>
2022-08-04 12:19:34 +0530

    home page first commit

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo home page'),
+      home: const MyHomePage(title: 'Flutter Demo page'),
     );
   }
 }
