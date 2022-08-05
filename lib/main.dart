4df596759bf0d7547704721b1c7704954cb7e1f5
Nisarg <nisargpatel1206@gmail.com>
2022-08-05 23:10:51 +0530

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,93 @@
-import 'dart:html';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/basic.dart';
-//import 'package:cura-1/main.dart';
+import 'package:flutter/src/material/card.dart';
+import 'package:flutter/src/material/text_button.dart';
+// import 'package:cura-1/main.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      body: 
-           Column(children: [
-        alignImage(),
-
-        // Align(alignment: Alignment(0,.5),
-        //     child :
-        //               Text('\nWelcome!',
-        //                    style:
-        //                          TextStyle
-        //                          (
-        //                            fontSize: 36,
-        //                            fontWeight: FontWeight.bold,
-        //                            //letterSpacing:3,
-        //                            color: Colors.black,
-        //                            fontFamily: 'PT Sans',
-
-        //                          )                    ),
-        //       ),
-        alignSplashscreen(),
-        textWelcome(),
-      ])
-    ),
+        body: Column(children: [ Container(
+      color: Colors.white,
+      child: Column(
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Row(children: <Widget>[
+                  Container(
+                    child: IconButton(
+                      icon: const Icon(
+                        Icons.arrow_back_ios_new,
+                        size: 15,
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ]))
+          ]))
+      ,
+      Center(
+        child: Text('Welcome!',
+            style: TextStyle(
+              fontSize: 25.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontFamily: 'PT Sans',
+            )),
+      ),
+      alignSplashscreen(),
+      
+      SizedBox(
+         child: 
+         Card(
+          color: Color.fromARGB(1, 142, 182, 192),
+          child:
+          Center(
+        child: Text('Email',
+            style: TextStyle(
+              fontSize: 25.0,
+              fontWeight: FontWeight.normal,
+              color: Colors.white,
+              fontFamily: 'PT Sans',
+            )),
+      ),),
+        height: 20,
+        width: 250,
+      ),
+      SizedBox(
+         child: 
+         Card(
+          color: Colors.white,
+          child:
+          Center(
+        child: Text('Phone',
+            style:TextStyle(
+              fontSize: 25.0,
+              fontWeight: FontWeight.normal,
+              color: Colors.white,
+              fontFamily: 'PT Sans',
+            )),
+      ),),
+        height: 20,
+        width: 250,
+      )
+      
+    
+  ])),
   ));
 }
 
-Align alignImage() {
-  return Align(
-      alignment: Alignment(-.98, -.8),
-      child: Image.asset(
-        'assets/startup_assets/preview_splash_assets/back_arrow.png',
-        height: 30,
-        width: 30,
-      ));
-}
 
 Align alignSplashscreen() {
   return Align(
-      alignment: Alignment(-.5, -2),
+      alignment: Alignment(-.5, .8),
       child: Image.asset(
         'assets/startup_assets/preview_splash_assets/spl4.png',
         height: 550,
         width: 320,
+        
       ));
 }
 
-textWelcome()
-{
-  return Text(
-         '\nWelcome!',
-                           style:
-                                 TextStyle
-                                 (
-                                   fontSize: 36,
-                                   fontWeight: FontWeight.bold,
-                                   //letterSpacing:3,
-                                   color: Colors.black,
-                                   fontFamily: 'PT Sans',
-                                 ));                   
-}
 
