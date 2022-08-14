432a3fcd3cfdfe82daa048fce7fa5eb30ad67c94
Nisarg <nisargpatel1206@gmail.com>
2022-08-14 14:56:06 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,13 +17,36 @@ class MyAppState extends State<MyApp> {
       home: Scaffold(
         body: Column(children: [
           Align(
-              alignment: const Alignment(-.5, .8),
+              // alignment: const Alignment(-.5, .8),
               child: Image.asset(
-                'assets/Mobile life-amico 2.png',
-                height: 350,
-                width: 350,
-                // alignment: Alignment.topCenter,
-              )),
+            'assets/Mobile life-amico 2.png',
+            height: 325,
+            width: 325,
+            // alignment: Alignment.topCenter,
+          )),
+          Container(
+            margin: EdgeInsets.all(0),
+            height: 40,
+            width: 400,
+            child: TextButton(
+              style: TextButton.styleFrom(
+                  shadowColor: Colors.black12,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(180),
+                  ),
+                  primary: const Color.fromARGB(255, 137, 184, 189),
+                  backgroundColor: Colors.white),
+              onPressed: (() {}),
+              child: Row(
+                children: [
+                  const Icon(
+                    Icons.search,
+                    size: 24,
+                  ),
+                ],
+              ),
+            ),
+          ),
         ]),
       ),
     );
