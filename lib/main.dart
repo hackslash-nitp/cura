b0ff8147cd388227fc279f8877ee9ad379704f3e
Nisarg <nisargpatel1206@gmail.com>
2022-08-14 14:07:27 +0530

    Final commit for welcome screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -16,105 +18,119 @@ class MyApp extends StatefulWidget {
 class MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      body: Column(children: [
-          Container(
-            // color: Colors.white,
-            child: Column(
-                // crossAxisAlignment: CrossAxisAlignment.center,
+        debugShowCheckedModeBanner: false,
+        home: SafeArea(
+          child: Scaffold(
+            body: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: Row(children: <Widget>[
-                        Container( 
-                          child: IconButton(
-                            icon: const Icon( 
-                              Icons.arrow_back_ios_new,
-                              size: 15,
+                      padding: const EdgeInsets.all(5.0),
+                      child: IconButton(
+                        icon: const Icon(
+                          Icons.arrow_back_ios_new,
+                          size: 24,
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      )),
+                  Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text('Welcome!',
+                            style: TextStyle(
+                              fontSize: 25.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                              fontFamily: 'PT Sans',
+                            )),
+                        Align(
+                            alignment: const Alignment(-.5, .8),
+                            child: Image.asset(
+                              'assets/startup_assets/preview_splash_assets/spl4a.png',
+                              height: 350,
+                              alignment: Alignment.topCenter,
+                            )),
+                        Container(
+                          margin: EdgeInsets.all(7),
+                          height: 50,
+                          width: 378,
+                          child: TextButton(
+                            onPressed: () {},
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(
+                                  Icons.email,
+                                  size: 24,
+                                ),
+                                const Text(
+                                  " Email",
+                                  style: TextStyle(
+                                      fontSize: 20, fontFamily: 'PT Sans'),
+                                ),
+                              ],
                             ),
-                            onPressed: () {
-                              print('Pressed');
-                            },
+                            // clipBehavior: (chi),
+                            style: TextButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              primary: Colors.white,
+                              backgroundColor:
+                                  const Color.fromARGB(255, 137, 184, 189),
+                            ),
                           ),
                         ),
-                      ]))
-                ])),
-        Center(
-          child: Text('Welcome!',
-              style: TextStyle(
-                fontSize: 25.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-                fontFamily: 'PT Sans',
-              )),
-        ),
-        Align(
-      alignment: Alignment(-.5, .8),
-      child: Image.asset(
-        'assets/startup_assets/preview_splash_assets/spl4a.png',
-        height: 460,
-        width: 260,
-        alignment: Alignment.topCenter,
-      )),
-        TextButton(
-          onPressed: (() {
-            print('Pressed');
-          }),
-          child: IconButton(
-            icon: const Icon(
-              Icons.email,
-              size: 15,
-            ),
-            onPressed: (() {}),
+                        Container(
+                          margin: EdgeInsets.all(7),
+                          height: 50,
+                          width: 378,
+                          child: TextButton(
+                            style: TextButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                primary:
+                                    const Color.fromARGB(255, 137, 184, 189),
+                                backgroundColor: Colors.white),
+                            onPressed: (() {}),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(
+                                  Icons.phone,
+                                  size: 15,
+                                ),
+                                const Text(
+                                  " Phone",
+                                  style: TextStyle(
+                                      fontSize: 20, fontFamily: 'PT Sans'),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        // const SizedBox(
+                        //   height: 10,
+                        // ),
+                        Center(
+                          child: Text('Terms & conditions',
+                              style: TextStyle(
+                                fontSize: 12.0,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.black,
+                                fontFamily: 'PT Sans',
+                              )),
+                        ),
+                      ],
+                    ),
+                  ),
+                ]),
           ),
-          // clipBehavior: (chi),
-          style: TextButton.styleFrom(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            primary: Colors.white,
-            backgroundColor: const Color.fromARGB(255, 137, 184, 189),
-          ),
-        ),
-        ElevatedButton(
-          style: TextButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              primary: const Color.fromARGB(255, 137, 184, 189),
-              backgroundColor: Colors.white),
-          onPressed: (() {
-            print('Pressed');
-          }),
-          child:IconButton(
-            icon: const Icon(
-              Icons.phone,
-              size: 15,
-            ),
-            onPressed: (() {}),
-          ),),
-          Center(
-          child: Text('Terms & conditions',
-              style: TextStyle(
-                fontSize: 12.0,
-                fontWeight: FontWeight.normal,
-                color: Colors.black,
-                fontFamily: 'PT Sans',
-              )),
-        ),
-      ]),
-    ));
+        ));
   }
 }
-
-Align alignSplashscreen() {
-  return Align(
-      alignment: Alignment(-.5, .8),
-      child: Image.asset(
-        'assets/startup_assets/preview_splash_assets/spl4a.png',
-        height: 450,
-        width: 250,
-        alignment: Alignment.topCenter,
-      ));
-}
-// Action(const Text('Email')),
