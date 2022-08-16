f0d5f825dbdb5bfdf8fc91a91e02cdb08f7a4106
RitwikSingh28 <itsritwik26@gmail.com>
2022-08-16 08:16:19 +0530

    add user models

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:cura/individual/home_page_individual.dart';
-import 'package:cura/individual/my_donations.dart';
-import 'package:cura/shared/screens/app_rating.dart';
 import 'package:cura/startup_screens/preview_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -27,7 +25,7 @@ class ScreenDecider extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       home: auth.getCurrentUser() == null
-          ? const AppRating() // directly access by substituting in place of PreviewPage()
+          ? PreviewPage() // directly access by substituting in place of PreviewPage()
           : const HomePageIndividual(),
     );
   }
