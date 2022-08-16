8d2403ba6c170204a5b381b61df1ae7a0a771cce
Ritwik Singh <106133014+RitwikSingh28@users.noreply.github.com>
2022-08-16 19:05:28 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:cura/individual/home_page_individual.dart';
-import 'package:cura/individual/support_team_page.dart';
 import 'package:cura/startup_screens/preview_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'individual/user_chats_page.dart';
 import 'shared/services/firebase_authentication.dart';
 
 Future<void> main() async {
@@ -26,13 +24,9 @@ class ScreenDecider extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-<<<<<<< HEAD
-      home:UserChatsPage(),
-=======
       home: auth.getCurrentUser() == null
           ? const PreviewPage() // directly access by substituting in place of PreviewPage()
           : const HomePageIndividual(),
->>>>>>> ccf11f6d91d69c07b0bc30c1f525a1dab56b1c57
     );
   }
 }
