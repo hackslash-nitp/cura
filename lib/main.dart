fb742fcd98dc437b54f6f485491c44e7b38c6c9b
Shazeb Khan <shazebk.ug21.cs@nitp.ac.in>
2022-08-17 14:24:22 +0530

    Add Donor Details Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cura/individual/donor_details.dart';
 import 'package:cura/individual/home_page_individual.dart';
 import 'package:cura/individual/support_team_page.dart';
 import 'package:cura/startup_screens/preview_page.dart';
@@ -26,13 +27,9 @@ class ScreenDecider extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-<<<<<<< HEAD
-      home:UserChatsPage(),
-=======
       home: auth.getCurrentUser() == null
-          ? PreviewPage() // directly access by substituting in place of PreviewPage()
+          ? DonerPage() // directly access by substituting in place of PreviewPage()
           : const HomePageIndividual(),
->>>>>>> ccf11f6d91d69c07b0bc30c1f525a1dab56b1c57
     );
   }
 }
