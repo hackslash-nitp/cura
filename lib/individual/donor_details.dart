7db7396c166c6cce295996b722c345a4a12013f9
Shazeb Khan <shazebk.ug21.cs@nitp.ac.in>
2022-08-17 14:29:21 +0530

    Edit Donor Details Page

--- lib/individual/donor_details.dart
+++ lib/individual/donor_details.dart
@@ -6,133 +6,125 @@ class DonerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.arrow_back_ios_new,
-                      size: 15.0,
-                    )),
-                Text(
-                  "Donor Detail",
-                  style: TextStyle(
-                    fontSize: 22.0,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                SizedBox(
-                  width: 48.0,
-                ),
-              ],
-            ),
-            CircleAvatar(
-              radius: 50.0,
-              child: Image.asset("assets/Heart.png"),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              "Shazeb",
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            Text("Last Connected 1 month ago",
-                style: TextStyle(
-                  color: Color(0xFF777474),
-                )),
-            Row(
+     body:SafeArea(
+       child:Column(
+         children: [
+           Row(
+             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+             children: [
+               IconButton(
+                   onPressed: () {},
+                   icon: Icon(
+                     Icons.arrow_back_ios_new,
+                     size: 15.0,
+                   )),
+               Text(
+                 "Donor Detail",
+                 style: TextStyle(
+                   fontSize: 22.0,
+                   fontWeight: FontWeight.w700,
+                 ),
+               ),
+               SizedBox(
+                 width: 48.0,
+               ),
+             ],
+           ),
+           CircleAvatar(
+               radius: 50.0,
+               child: Image.asset("assets/Heart.png"),
+           ),
+           SizedBox(height: 10.0),
+           Text(
+             "Shazeb",
+             style:TextStyle(
+               fontSize: 24.0,
+               fontWeight: FontWeight.w700,
+             ),
+           ),
+           Text(
+               "Last Connected 1 month ago",
+               style:TextStyle(
+                 color: Color(0xFF777474),
+               )
+             ),
+           Row(
               children: [
                 Expanded(
                   child: Container(
                     decoration: BoxDecoration(
-                      color: Color(0xFFE0EEEF),
+                      color:Color(0xFFE0EEEF),
                       borderRadius: BorderRadius.circular(10.0),
                     ),
-                    padding: EdgeInsets.all(10.0),
-                    margin: EdgeInsets.all(10.0),
-                    child: Text(
-                      "+1 879 944-4489",
-                      textAlign: TextAlign.center,
-                    ),
+                    padding:EdgeInsets.all(10.0),
+                    margin:EdgeInsets.all(10.0),
+                    child: Text("+1 879 944-4489",textAlign: TextAlign.center,),
                   ),
                 ),
                 Expanded(
                   child: Container(
                     decoration: BoxDecoration(
-                      color: Color(0xFFE0EEEF),
+                      color:Color(0xFFE0EEEF),
                       borderRadius: BorderRadius.circular(10.0),
                     ),
-                    padding: EdgeInsets.all(10.0),
-                    margin: EdgeInsets.all(10.0),
-                    child: Text(
-                      "shazeb12@gmail.com",
-                      textAlign: TextAlign.center,
-                    ),
+                    padding:EdgeInsets.all(10.0),
+                    margin:EdgeInsets.all(10.0),
+                    child: Text("shazeb12@gmail.com",textAlign: TextAlign.center,),
                   ),
                 ),
               ],
             ),
-            SizedBox(height: 10.0),
-            Text(
-              "Personal Information",
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
+           SizedBox(height: 10.0),
+           Text(
+             "Personal Information",
+             style:TextStyle(
+               fontSize: 20.0,
+               fontWeight: FontWeight.w700,
+             ),
+           ),
             Expanded(
-              flex: 3,
+              flex:3,
               child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 15.0),
+                padding: EdgeInsets.symmetric(horizontal: 10.0,vertical: 15.0),
                 margin: EdgeInsets.all(10.0),
                 decoration: BoxDecoration(
                   color: Color(0xFFE0EEEF),
                   borderRadius: BorderRadius.circular(15.0),
                 ),
-                child: Column(
-                  children: [
-                    TwoTexts("Date of birth", "12 May 2022"),
-                    TwoTexts("Related Contacts", "Wife:Angela Jolie"),
-                    TwoTexts("Title", "Assistant Manager"),
-                    TwoTexts("Department", "Management"),
-                    TwoTexts(
-                        "Mailing Address", "9945 Watthem Street,NewYork,USA"),
-                    TwoTexts("Company", "Best Buy"),
-                    TwoTexts(
-                        "Company Address", "3345 Block Rd Ave,Austi TX 78499"),
-                  ],
+                child: Column(children: [
+                  TwoTexts("Date of birth", "12 May 2022"),
+                  TwoTexts("Related Contacts","Wife:Angela Jolie"),
+                  TwoTexts("Title","Assistant Manager"),
+                  TwoTexts("Department","Management"),
+                  TwoTexts("Mailing Address","9945 Watthem Street,NewYork,USA"),
+                  TwoTexts("Company","Best Buy"),
+                  TwoTexts("Company Address","3345 Block Rd Ave,Austi TX 78499"),
+                ],
                 ),
               ),
             ),
-            Text(
-              "Administrative Details",
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 15.0),
-                margin: EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  color: Color(0xFFE0EEEF),
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-                child: Column(
-                  children: [
-                    TwoTexts("Added", "14 Dec 2019"),
-                    TwoTexts("Total Donations", "\$  5522"),
-                  ],
-                ),
-              ),
-            ),
+           Text(
+             "Administrative Details",
+             style:TextStyle(
+               fontSize: 20.0,
+               fontWeight: FontWeight.w700,
+             ),
+           ),
+           Expanded(
+             child: Container(
+               padding: EdgeInsets.symmetric(horizontal: 10.0,vertical: 15.0),
+               margin: EdgeInsets.all(10.0),
+               decoration: BoxDecoration(
+                 color: Color(0xFFE0EEEF),
+                 borderRadius: BorderRadius.circular(15.0),
+               ),
+               child: Column(children: [
+                 TwoTexts("Added", "14 Dec 2019"),
+                 TwoTexts("Total Donations","\$  5522"),
+               ],
+               ),
+             ),
+           ),
           ],
         ),
       ),
@@ -141,10 +133,11 @@ class DonerPage extends StatelessWidget {
 }
 
 class TwoTexts extends StatelessWidget {
+
   String heading;
   String text;
 
-  TwoTexts(this.heading, this.text);
+  TwoTexts(this.heading,this.text);
 
   @override
   Widget build(BuildContext context) {
@@ -165,7 +158,7 @@ class TwoTexts extends StatelessWidget {
           Expanded(
             child: Text(
               text,
-              textAlign: TextAlign.end,
+      textAlign: TextAlign.end,
               style: TextStyle(
                 fontSize: 14.0,
                 fontWeight: FontWeight.w400,
@@ -177,3 +170,5 @@ class TwoTexts extends StatelessWidget {
     );
   }
 }
+
+
